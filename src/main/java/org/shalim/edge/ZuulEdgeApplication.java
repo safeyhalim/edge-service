4ca3114151ba1ec40d1a3301ee4651edc5f1c8ef
safeyhalim <safey.allah@gmail.com>
2021-04-14 17:46:54 +0200

    Project basic setup

---
+++ src/main/java/org/shalim/edge/ZuulEdgeApplication.java
@@ -0,0 +1,13 @@
+package org.shalim.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulEdgeApplication.class, args);
+	}
+
+}
